3fe6278dae56d0508ad51b2ce9d00efc62a90b70
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2026-03-11 11:12:20 +0100

    Apply consistent brace style and formatting to liveqmlengine.cpp

--- DesignPattern/liveqmlengine.cpp
+++ DesignPattern/liveqmlengine.cpp
@@ -1,139 +1,129 @@
 #include "liveqmlengine.h"
 
-//using namespace DesignPattern;
+// using namespace DesignPattern;
 
 Q_LOGGING_CATEGORY(liveengineerror, "liveengine.componentError")
 Q_LOGGING_CATEGORY(liveenginelog, "liveengine.log")
 
-QStringList LiveQmlEngine::qmlSourceDir() const
-{
-    return m_qmlSourceDir;
-}
+QStringList LiveQmlEngine::qmlSourceDir() const { return m_qmlSourceDir; }
 
-void LiveQmlEngine::setQmlSourceDir(QStringList qmlSourceDir)
-{
-    m_qmlSourceDir = qmlSourceDir;
+void LiveQmlEngine::setQmlSourceDir(QStringList qmlSourceDir) {
+  m_qmlSourceDir = qmlSourceDir;
 }
 
 LiveQmlEngine::LiveQmlEngine(QObject *parent, QStringList sourceDir)
-    : QObject(parent), m_qmlSourceDir(sourceDir)
-{
-    auto *context = qmlEngine().rootContext();
-    context->setContextProperty("_liveQmlEngine", this);
+    : QObject(parent), m_qmlSourceDir(sourceDir) {
+  auto *context = qmlEngine().rootContext();
+  context->setContextProperty("_liveQmlEngine", this);
 #ifdef ENABLE_HOTRELOADING
 
-    connect(&m_watcher, &QFileSystemWatcher::fileChanged, this, &LiveQmlEngine::onFileChanged);
-    connect(&m_watcher, &QFileSystemWatcher::directoryChanged, this, &LiveQmlEngine::onFileChanged);
+  connect(&m_watcher, &QFileSystemWatcher::fileChanged, this,
+          &LiveQmlEngine::onFileChanged);
+  connect(&m_watcher, &QFileSystemWatcher::directoryChanged, this,
+          &LiveQmlEngine::onFileChanged);
 
-    for(auto it: qmlSourceDir()) {
-        QDir dir(it);
-        QList<QFileInfo> list = dir.entryInfoList(QDir::AllDirs | QDir::NoDotDot);
+  for (auto it : qmlSourceDir()) {
+    QDir dir(it);
+    QList<QFileInfo> list = dir.entryInfoList(QDir::AllDirs | QDir::NoDotDot);
 
-        while (!list.isEmpty()) {
-            dir.cd(list.first().absoluteFilePath());
-            qCDebug(liveenginelog) << dir.absolutePath();
-            auto infoList = dir.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot);
-            for (auto it : infoList) {
-                if (it.isDir()) {
-                    list << it;
-                } else if (it.suffix() == "qml") {
-                    m_watcher.addPath(it.absoluteFilePath());
-                }
-            }
-            list.pop_front();
+    while (!list.isEmpty()) {
+      dir.cd(list.first().absoluteFilePath());
+      qCDebug(liveenginelog) << dir.absolutePath();
+      auto infoList =
+          dir.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot);
+      for (auto it : infoList) {
+        if (it.isDir()) {
+          list << it;
+        } else if (it.suffix() == "qml") {
+          m_watcher.addPath(it.absoluteFilePath());
         }
+      }
+      list.pop_front();
     }
-    qCDebug(liveenginelog) << m_watcher.files();
+  }
+  qCDebug(liveenginelog) << m_watcher.files();
 #endif
 }
 
-void LiveQmlEngine::createWindow(QUrl path, QQmlContext* context, QUuid id)
-{
-    QVariantMap properties;
+void LiveQmlEngine::createWindow(QUrl path, QQmlContext *context, QUuid id) {
+  QVariantMap properties;
 
-    QString source = path.toString();
-#ifdef ENABLE_HOTRELOADING //Setup qml source dir
-    for(auto it: qmlSourceDir())
-        if(QFile::exists(it + source))
-            source.prepend(it);
-    source.prepend("file:///");
+  QString source = path.toString();
+#ifdef ENABLE_HOTRELOADING // Setup qml source dir
+  for (auto it : qmlSourceDir())
+    if (QFile::exists(it + source))
+      source.prepend(it);
+  source.prepend("file:///");
 #else
-    source.prepend("qrc:");
+  source.prepend("qrc:");
 #endif
 
-    if (m_windows.contains(id)) {
-        properties["visible"] = m_windows[id]->property("visible");
-        delete m_windows[id];
+  if (m_windows.contains(id)) {
+    properties["visible"] = m_windows[id]->property("visible");
+    delete m_windows[id];
+  }
+
+  auto comp = new QQmlComponent(&m_engine, QUrl(source));
+  if (id.isNull()) {
+    id = QUuid::createUuid();
+  }
+
+  if (!m_ids.contains(path, id))
+    m_ids.insert(path, id);
+
+  m_context[id] = context;
+  QObject *item;
+  if (properties.isEmpty())
+    item = comp->create(context);
+  else
+    item = comp->createWithInitialProperties(properties, context);
+
+  onObjectCreated(item, QUrl(source), id);
+  qCDebug(liveengineerror) << path << comp->errorString();
+}
+
+void LiveQmlEngine::onObjectCreated(QObject *window, QUrl url, QUuid id) {
+  m_windows[id] = window;
+  connect(window, &QObject::destroyed, this, &LiveQmlEngine::onDestroyed);
+  emit sObjectCreated(id, url, window);
+}
+
+void LiveQmlEngine::onDestroyed(QObject *window) {
+  auto id = m_windows.key(window);
+  m_windows.remove(id);
+  m_context.remove(id);
+  auto url = m_ids.key(id);
+  m_ids.remove(url, id);
+  emit sObjectDestroyed(id);
+}
+
+void LiveQmlEngine::onFileChanged(QString path) {
+  m_engine.clearComponentCache();
+
+  for (auto it : m_ids.keys()) {
+    auto source = it.toString();
+    source.remove("file:///");
+    for (auto it2 : qmlSourceDir())
+      if (source.startsWith(it2))
+        source.remove(0, it2.size());
+
+    for (auto it3 : m_ids.values(it)) {
+      createWindow(QUrl(source), m_context[it3], it3);
     }
+  }
 
-    auto comp = new QQmlComponent(&m_engine, QUrl(source));
-    if(id.isNull()) {
-        id = QUuid::createUuid();
-    }
-
-    if(!m_ids.contains(path, id))
-        m_ids.insert(path, id);
-
-    m_context[id] = context;
-    QObject* item;
-    if(properties.isEmpty())
-        item = comp->create(context);
-    else
-        item = comp->createWithInitialProperties(properties, context);
-
-    onObjectCreated(item, QUrl(source), id);
-    qCDebug(liveengineerror)<<path<<comp->errorString();
+  if (QFile::exists(path)) {
+    m_watcher.addPath(path);
+  }
 }
 
-void LiveQmlEngine::onObjectCreated(QObject *window, QUrl url, QUuid id)
-{
-    m_windows[id] = window;
-    connect(window, &QObject::destroyed, this, &LiveQmlEngine::onDestroyed);
-    emit sObjectCreated(id, url, window);
-}
+QQmlApplicationEngine &LiveQmlEngine::qmlEngine() { return m_engine; }
 
-void LiveQmlEngine::onDestroyed(QObject *window)
-{
-    auto id = m_windows.key(window);
-    m_windows.remove(id);
-    m_context.remove(id);
-    auto url = m_ids.key(id);
-    m_ids.remove(url, id);
-    emit sObjectDestroyed(id);
-}
-
-void LiveQmlEngine::onFileChanged(QString path)
-{
-    m_engine.clearComponentCache();
-
-    for (auto it : m_ids.keys()) {
-        auto source = it.toString();
-        source.remove("file:///");
-        for(auto it2: qmlSourceDir())
-            if(source.startsWith(it2))
-                source.remove(0, it2.size());
-
-        for(auto it3: m_ids.values(it)) {
-            createWindow(QUrl(source), m_context[it3], it3);
-        }
-    }
-
-    if (QFile::exists(path)) {
-        m_watcher.addPath(path);
-    }
-}
-
-QQmlApplicationEngine &LiveQmlEngine::qmlEngine()
-{
-    return m_engine;
-}
-
-void LiveQmlEngine::removeWindow(QObject* windows)
-{
-    auto id = m_windows.key(windows);
-    auto url = m_ids.key(id);
-    m_ids.remove(url, id);
-    m_context.remove(id);
-    m_windows.remove(id);
-    windows->deleteLater();
+void LiveQmlEngine::removeWindow(QObject *windows) {
+  auto id = m_windows.key(windows);
+  auto url = m_ids.key(id);
+  m_ids.remove(url, id);
+  m_context.remove(id);
+  m_windows.remove(id);
+  windows->deleteLater();
 }
